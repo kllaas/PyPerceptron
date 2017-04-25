d47492e043d3c05e69a67a46445d7f5b56ef8b47
kllaas <kll.a.s@yandex.ru>
2017-04-25 22:38:16 +0300

    add test data

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from numpy.matlib import rand
 
 from perceptron import Perceptron
 
-
-df = pd.read_csv('D:\Мои документы\Алёша\Python Projects\PyPerceptron\data05.csv', header=None)
+df = pd.read_csv('/home/alexey/PycharmProjects/PyPerceptron/data05.csv', header=None)
 
 # setosa and versicolor
 y = df.iloc[1:78, 2].values
@@ -20,6 +19,19 @@ p = Perceptron(epochs=100, learning_rate=0.01)
 
 p.train(X, y)
 print('Weights: %s' % p.weights)
+
+df = pd.read_csv('/home/alexey/PycharmProjects/PyPerceptron/test_data.csv', header=None)
+
+# setosa and versicolor
+y = df.iloc[1:21, 2].values
+
+# sepal length and petal length
+X = df.iloc[1:21, [0, 1]].values
+
+print 'Test set:'
+
+for i in range(1, len(X)):
+    print ' %i/%i) Prdeict: %r' % (i, len(X), p.predict(X[i]) == y[i])
 
 plot_decision_regions(X, y, clf=p)
 plt.title('Perceptron')
