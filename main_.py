769e42ea0679822eb9c0bcdef99a5516661bbd54
kllaas <kll.a.s@yandex.ru>
2017-04-26 23:28:52 +0300

    adding comments

--- main_.py
+++ main_.py
@@ -8,6 +8,7 @@ from mlxtend.plotting import plot_decision_regions
 
 from perceptron import Perceptron
 
+# reading training data
 df = pd.read_csv('/home/alexey/PycharmProjects/PyPerceptron/data05.csv', header=None)
 
 y = df.iloc[1:78, 2].values
@@ -18,7 +19,7 @@ p = Perceptron(epochs=100, learning_rate=0.01)
 p.train(X, y)
 print('Weights: %s' % p.weights)
 
-
+# reading test data
 df = pd.read_csv('/home/alexey/PycharmProjects/PyPerceptron/test_data.csv', header=None)
 
 y = df.iloc[1:21, 2].values
@@ -29,6 +30,7 @@ print 'Test set:'
 for i in range(1, len(X)):
     print ' %i/%i) Prdeict: %r' % (i, len(X), p.predict(X[i]) == y[i])
 
+# plot results
 plot_decision_regions(X, y, clf=p)
 plt.title('Perceptron')
 plt.xlabel('firstParam')
--- perceptron.py
+++ perceptron.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class Perceptron(object):
-    def __init__(self, learning_rate=0.01, epochs=50, threshold=0.5):
+    def __init__(self, learning_rate=0.01, epochs=50, threshold=0.5, epsilon=0):
         self.learning_rate = learning_rate
         self.threshold = threshold
         self.epochs = epochs
+        self.epsilon = epsilon
 
     def train(self, X, y):
         # self.weights = [0.333206, 0.038794]
@@ -13,22 +14,29 @@ class Perceptron(object):
         self.weights = np.zeros(X.shape[1])
         self.errors_ = []
 
-        for _ in range(self.epochs):
+        for e in range(self.epochs):
             total_error = 0
 
             for xi, answer in zip(X, y):
                 error = answer - self.predict(xi)
 
+                # calculate new weights
                 for i, item in enumerate(self.weights):
                     self.weights[i] += self.learning_rate * error * xi[i]
 
+                # add error from current iteration to total_error
                 total_error += abs(answer - self.predict(xi))
 
             self.errors_.append(total_error)
-            print 'Epoch: %d, Error: %f' % (_, total_error)
+            print 'Epoch: %d, Error: %f' % (e, total_error)
+
+            # break if error is less then epsilon
+            if total_error <= self.epsilon:
+                break
 
         return self
 
+    # output = W1*X1+W2+X2
     def output(self, X):
         return np.dot(X, self.weights[:])
 
